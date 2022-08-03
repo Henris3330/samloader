c5f9ac4786835587f6b2f585c30b5f0150302b61
Henris3330 <csgo40055@gmail.com>
2022-08-03 11:34:36 +0300

    error handle missing stuff

--- samloader/main.py
+++ samloader/main.py
@@ -27,19 +27,21 @@ def main():
             csc = line.split("csc: ")[1].split(" fw_version:")[0]
             old_fw_version = line.split("fw_version: ")[1].split(" os_version:")[0]
             path, filename, size, fw_version, os_version = getbinaryfile(client, old_fw_version, subfolder[2:], csc)
-            for str in filename.split("_"):
-                if len(str) == 14:
-                    build_date = str
-                    break
-            if int(build_date) > int(latest_date):
-                latest = fw_version
-                latest_csc = csc
-                latest_date = build_date
-            fw_list.append("csc: " + csc + " fw_version: " + fw_version + " os_version: " + os_version + " build_date: " + build_date)
-            if "6." in os_version:
-                fw_list_mm.append("csc: " + csc + " fw_version: " + fw_version + " os_version: " + os_version + " build_date: " + build_date)
-            if "7." in os_version:
-                fw_list_nn.append("csc: " + csc + " fw_version: " + fw_version + " os_version: " + os_version + " build_date: " + build_date)
+            # garbage error handle
+            if old_fw_version != fw_version:
+                for str in filename.split("_"):
+                    if len(str) == 14:
+                        build_date = str
+                        break
+                if int(build_date) > int(latest_date):
+                    latest = fw_version
+                    latest_csc = csc
+                    latest_date = build_date
+                fw_list.append("csc: " + csc + " fw_version: " + fw_version + " os_version: " + os_version + " build_date: " + build_date)
+                if "6." in os_version:
+                    fw_list_mm.append("csc: " + csc + " fw_version: " + fw_version + " os_version: " + os_version + " build_date: " + build_date)
+                if "7." in os_version:
+                    fw_list_nn.append("csc: " + csc + " fw_version: " + fw_version + " os_version: " + os_version + " build_date: " + build_date)
         info.seek(0)
         info.truncate()
         info.write("\n".join(fw_list))
@@ -71,16 +73,22 @@ def getbinaryfile(client, fw, model, region):
     resp = client.makereq("NF_DownloadBinaryInform.do", req)
     root = ET.fromstring(resp)
     status = int(root.find("./FUSBody/Results/Status").text)
-    if status != 200:
-        raise Exception("DownloadBinaryInform returned {}, firmware could not be found?".format(status))
-    filename = root.find("./FUSBody/Put/BINARY_NAME/Data").text
-    if filename is None:
-        raise Exception("DownloadBinaryInform failed to find a firmware bundle")
-    size = int(root.find("./FUSBody/Put/BINARY_BYTE_SIZE/Data").text)
-    path = root.find("./FUSBody/Put/MODEL_PATH/Data").text
-    # ADD_LATEST_FW_VERSION, ADD_LATEST_DISPLAY_VERSION -> latest u can get by clicking update in phone while having "old_vc"
-    # CURRENT_DISPLAY_VERSION -> fw we pinged with
-    # LATEST_DISPLAY_VERSION -> newest firmware, same as LATEST_FW_VERSION ???
-    fw_version = root.find("./FUSBody/Results/LATEST_FW_VERSION/Data").text
-    os_version = root.find("./FUSBody/Put/LATEST_OS_VERSION/Data").text
+    if status == 200:
+        filename = root.find("./FUSBody/Put/BINARY_NAME/Data").text
+        if filename is None:
+            raise Exception("DownloadBinaryInform failed to find a firmware bundle")
+        size = int(root.find("./FUSBody/Put/BINARY_BYTE_SIZE/Data").text)
+        path = root.find("./FUSBody/Put/MODEL_PATH/Data").text
+        # ADD_LATEST_FW_VERSION, ADD_LATEST_DISPLAY_VERSION -> latest u can get by clicking update in phone while having "old_vc"
+        # CURRENT_DISPLAY_VERSION -> fw we pinged with
+        # LATEST_DISPLAY_VERSION -> newest firmware, same as LATEST_FW_VERSION ???
+        fw_version = root.find("./FUSBody/Results/LATEST_FW_VERSION/Data").text
+        os_version = root.find("./FUSBody/Put/LATEST_OS_VERSION/Data").text
+    else:
+        print(model + " " + region)
+        path = "asdf"
+        filename = "asdf"
+        size = 0
+        fw_version = fw
+        os_version = "6"
     return path, filename, size, fw_version, os_version
